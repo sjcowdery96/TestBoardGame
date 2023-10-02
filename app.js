1ecda3608250dffcf88c7351809a1b96762483b3
sjcowdery96 <sjc.cowdery@gmail.com>
2023-10-02 11:26:45 -0500

    dragDrop fixed

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const gameBoard = document.querySelector("#gameboard")
 const playerOnDeck = document.querySelector("#playerOnDeck") //span element
 const infoDisplay = document.querySelector("#infoDisplay")
 
-const scores = [0,0]
-const piecesLeft = [36,36,4,4,1] //defines 36 seed pieces each, 4 deserts each, 1 koya
+const scores = [0, 0]
+const piecesLeft = [36, 36, 4, 4, 1] //defines 36 seed pieces each, 4 deserts each, 1 koya
 
 
 const width = 9 //meaning 0-8 indexes
@@ -12,34 +12,34 @@ let playerTurn = 'white' //global variable for whose turn
 playerOnDeck.textContent = 'white' //sets the display span to be white
 
 const startPieces = [
-    sampleSeed, sampleSeed, sampleSeed, sampleSeed, sampleSeed, sampleSeed, sampleSeed, sampleSeed, sampleSeed, 
-    '','','','','','','','','',
-    '','','','','','','','','',
-    '','','','','','','','',otherTree,
-    '','','','','','','','','',
-    '','','',otherSeed,'','','','','',
-    '','','','','','','','','',
-    '','','','','','','','','',
+    sampleSeed, sampleSeed, sampleSeed, sampleSeed, sampleSeed, sampleSeed, sampleSeed, sampleSeed, sampleSeed,
+    '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', otherTree,
+    '', '', '', '', '', '', '', '', '',
+    '', '', '', otherSeed, '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '', '',
     singlePine, singlePine, singlePine, singlePine, singlePine, singlePine, singlePine, singlePine, singlePine,
 ]
 
-function createBoard(){
-    startPieces.forEach((startPiece,i)=> {
+function createBoard() {
+    startPieces.forEach((startPiece, i) => {
         const square = document.createElement("div")
         square.classList.add("square")
         square.setAttribute("square-id", i)
         square.innerHTML = startPiece
         square.firstChild?.setAttribute('draggable', true)
-        
-        if(i%2===0){
+
+        if (i % 2 === 0) {
             square.classList.add("accentColor")
         }
         //these throw errors if the space does not have an SVG to fill -- the error is for no first child
-        if(i <= 8){
+        if (i <= 8) {
             square.firstChild?.firstChild.classList.add("p1Color")
         }
 
-        if(i >= 72){
+        if (i >= 72) {
             square.firstChild?.firstChild.classList.add("p2Color")
         }
         gameBoard.append(square)
@@ -53,9 +53,11 @@ createBoard()
 //selects all squares
 const allSquares = document.querySelectorAll("#gameBoard .square")
 
-allSquares.forEach(square=>{
+allSquares.forEach(square => {
+    //adds event listeners 
     square.addEventListener('dragstart', dragStart)
-    square.addEventListener('draggend', dragOver)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)//listen for drop event
 })
 
 //define global variables for position and dragged element
@@ -63,44 +65,57 @@ let startPositionID
 let draggedElement
 let endDraggedPosition
 
-function dragStart(e){
-  startPositionID = e.target.parentNode.getAttribute('square-id')
-  //draggedElement = e.target
-  console.log(startPositionID)
+function dragStart(e) {
+    startPositionID = e.target.parentNode.getAttribute('square-id')
+    draggedElement = e.target
+
+    console.log("dragStart " + startPositionID + " \n" + draggedElement)
+
 }
 
-function dragOver(e){
-    //e.preventDefault()
+function dragOver(e) {
+    e.preventDefault()//prevents from constantly listening for this
     endDraggedPosition = e.target.parentNode.getAttribute('square-id')
-    console.log(endDraggedPosition)
+    //console.log(endDraggedPosition)
 }
 
-function changePlayer(){
-    if(playerOnDeck === 'white'){
-        playerOnDeck = 'black'
-        playerTurn.textContent = "black"
+function changePlayer() {
+    if (playerTurn === 'white') {
+        playerTurn = 'black'
+        playerOnDeck.textContent = "black"
     }
-    else{
-        playerOnDeck = 'white'
-        playerTurn.textContent = 'white'
+    else {
+        playerTurn = 'white'
+        playerOnDeck.textContent = 'white'
     }
     console.log("changed player")
+    //flips board
+    flipBoard()
 }
 
-function dragDrop(e){
-    e.stopPropogation()
-    //define some logic for replacing and taking pieces 
+function dragDrop(e) {
+    //prevemtoimg doubling
+    e.preventDefault()
+
+    //if there is a piece in the target drop zone, define taken as not null
     const taken = e.target.classList.contains('piece')
-    console.log(e)
-    //e.target.remove()
-    //e.target.parentNode.append(draggedElement) //parentNode is grabbing the parent node of the piece...meaning the square
-    //e.target.append(draggedElement)
+
+    if (taken) {
+        //append new piece then remove the current piece 
+        e.target.parentNode.append(draggedElement)
+        e.target.remove()
+
+    }
+    else {
+        //no piece present = blank space
+        e.target.append(draggedElement)
+    }
+    //change player
     changePlayer()
 
 }
 
-
-function checkBranch(){
+function checkBranch() {
     //checks if a branch has been completed, to create a root
 
     //check branch left
@@ -114,14 +129,15 @@ function checkBranch(){
     //check diagonals
 }
 
-function placePiece(player, location){ //plays a piece   
+function placePiece(player, location) {
+    //plays a piece   
     //checks if square is occupied
-    if(location.firstChild!=null){
+    if (location.firstChild != null) {
         //there is a piece there
         console.log("that move is invalid!")
     }
-    else{
-       console.log("no piece there!")
+    else {
+        console.log("no piece there!")
     }
 
     //if not, place piece 
@@ -131,3 +147,28 @@ function placePiece(player, location){ //plays a piece
     //updates score and other things
 }
 
+function updateScore() {
+    //scans the board and updates the score
+
+}
+
+function isTrunk() {
+    //checks if we created a tree
+}
+
+//flips the board around -- only useful if two players on one device
+function flipBoard() {
+    const allSquares = document.querySelectorAll(".square")
+    if (playerTurn === "white") {
+        allSquares.forEach((square, i) => {
+            square.setAttribute('square-id', (width * width - 1) - i) //clever math to reverse id's
+        })
+    }
+    else {
+        allSquares.forEach((square, i) => {
+            square.setAttribute('square-id', i) //clever math to re-reverse id's
+        })
+    }
+
+
+}
--- readme.txt
+++ readme.txt
@@ -2,3 +2,9 @@
 This is a WIP for a boardgame I have built in the real world
 Drawing inspiration for structure from a youtube tutorial on building Chess --> https://www.youtube.com/watch?v=Qv0fvm5B0EM&t=2100s
 
+10.09.2023
+- fixed draggable pieces 
+- still chess underneath 
+LEFT TO DO
+- addPiece function
+-  
